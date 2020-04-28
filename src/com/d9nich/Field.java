068d65f831ad0e94fd44681b8bd90cac3a82b890
Danylo Halaiko <dan.galaiko@gmail.com>
2020-04-28 10:44:43 +0300

    New class Field has been written

---
+++ src/com/d9nich/Field.java
@@ -0,0 +1,66 @@
+package com.d9nich;
+
+import java.io.Serializable;
+
+public class Field implements Serializable, Comparable<Field> {
+    private static int defaultKey = 0;
+    private int key;
+    private String value;
+
+    public Field(String value) {
+        this(defaultKey++, value);
+    }
+
+    public Field(int key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public static int getDefaultKey() {
+        return defaultKey;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Field field = (Field) o;
+
+        return getKey() == field.getKey();
+    }
+
+    @Override
+    public int hashCode() {
+        return getKey();
+    }
+
+    @Override
+    public int compareTo(Field field) {
+        return Integer.compare(key, field.key);
+    }
+
+    @Override
+    public String toString() {
+        return "Field{" +
+                "key=" + key +
+                ", value='" + value + '\'' +
+                '}';
+    }
+}
